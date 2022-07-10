8def10030bdb3778db8c7b3c64de0cf4094fd6c0
Justin Stone <justinstone@Justins-MacBook-Pro-2.local>
2022-07-09 20:50:52 -0400

    Set up my generateHTML template

--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -1,4 +1,27 @@
 const template = (data) => {
     return `
-    `
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Members</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+    integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="./output/style.css">
+</head>
+<h1 class ="m-1 page-header">
+    My Team
+</h1>
+<body>
+<div class="container">
+<div class="holder justify-content-center row">
+    ${data}
+</div>
+</div>
+</body>
+</html>
+`
 }
